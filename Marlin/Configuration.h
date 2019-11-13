f6de00495f0bf788e3dc265c368ae12f9cbbd142
Peter Sotnikov <peter.sotnikov@gmail.com>
2019-11-13 10:35:02 +0300

    z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1068,7 +1068,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 264 // - 3.40
+#define Z_MAX_POS 263.8 // - 3.40
 
 /**
  * Software Endstops
